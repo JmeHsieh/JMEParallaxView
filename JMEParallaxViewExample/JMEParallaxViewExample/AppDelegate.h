f0b9b205a1899d2ca5a83e9b80db796ae79e7b82
JmeHsieh <jmehsieh@gmail.com>
2014-05-09 22:02:25 +0800

    Add JMEParallaxViewExample project

---
+++ JMEParallaxViewExample/JMEParallaxViewExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JMEParallaxViewExample
+//
+//  Created by Jme on 5/8/14.
+//  Copyright (c) 2014 JmeHsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JMEParallaxViewExample/JMEParallaxViewExample/ParallaxTableViewController.m
@@ -0,0 +1,92 @@
+//
+//  ParallaxTableViewController.m
+//  JMEParallaxView
+//
+//  Created by Jme on 5/8/14.
+//  Copyright (c) 2014 JmeHsieh. All rights reserved.
+//
+
+#import "ParallaxTableViewController.h"
+#import "JMEParallaxView.h"
+
+static const NSInteger kPhotosCount = 20;
+static const CGFloat kPadding = 10;
+static const CGFloat kImageHeight = 130;
+static const CGFloat kRowHeight = 2*kPadding + kImageHeight;
+
+@interface ParallaxTableViewController ()
+@property (strong, nonatomic) NSArray *photos;
+@end
+
+@implementation ParallaxTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    if (self = [super initWithStyle:style]) {
+        NSMutableArray *photos = [NSMutableArray arrayWithCapacity:kPhotosCount];
+        for (int i=0; i<kPhotosCount; ++i) {
+            photos[i] = [NSString stringWithFormat:@"%d.jpg", i];
+        }
+        self.photos = [NSArray arrayWithArray:photos];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"Photos";
+    self.tableView.rowHeight = kRowHeight;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.photos.count;
+}
+
+#pragma mark 0 UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.backgroundColor = [UIColor clearColor];
+        
+        JMEParallaxView *parallaxView = [[JMEParallaxView alloc] initWithFrame:CGRectMake(0, kPadding, 320, kImageHeight)];
+        parallaxView.tag = 111;
+        parallaxView.direction = JMEParallaxVertical;
+        parallaxView.reversedContentRevealing = YES;
+        parallaxView.contentDisplayingPercentage = 0.6;
+        parallaxView.anchorOffset = 64.0;   // set this to 0 if you run in iOS6 simulator.
+        parallaxView.observingScrollView = tableView;
+        
+        [cell.contentView addSubview:parallaxView];
+    }
+    
+    JMEParallaxView *parallaxView = (JMEParallaxView *)[cell viewWithTag:111];
+    parallaxView.contentImageView.image = [UIImage imageNamed:self.photos[indexPath.row]];
+    parallaxView.activeRange = [JMEParallaxView activeRangeWithTableView:tableView
+                                                               indexPath:indexPath
+                                                               direction:parallaxView.direction
+                                                              edgeInsets:UIEdgeInsetsMake(kPadding, 0, kPadding, 0)];
+    
+    return cell;
+}
+
+@end
